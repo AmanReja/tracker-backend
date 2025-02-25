9285e9ae0b5710c949fb4f99fa0926c3c726f6e5
Aman Reja <amnmolla742@gmail.com>
2025-02-25 18:28:06 +0530

    fix

--- index.js
+++ index.js
@@ -16,12 +16,6 @@ const io = socketIo(server, {
 });
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
-});
-
 io.on("connection", (socket) => {
   console.log("a user connected", socket.id);
 
